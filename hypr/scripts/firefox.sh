da4a7bb46d0e4e0a76e24306aa95b2e3bc7733b5
MaelBriantin <mael.briantin@proton.me>
2025-05-03 12:58:32 +0200

    add hyprlock config and update waybar style

---
+++ hypr/scripts/firefox.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Define the target application executable name
+APP_NAME="firefox-developer-edition"
+
+# Use pgrep -f to match against the full command line, avoiding the 15-char limit
+if [[ $(pgrep -f -x "$APP_NAME") ]]; then
+  echo "$APP_NAME is running"
+  exit 1
+fi
+
+hyprctl dispatch exec "uwsm app -- $APP_NAME"
+
+# Use pgrep -f to match against the full command line, avoiding the 15-char limit
+if [[ $(pgrep -f -x "$APP_NAME") ]]; then
+  echo "$APP_NAME is running"
+  exit 1
+fi
+
+for _ in {1..100}; do
+  addr=$(hyprctl clients -j | jq -r --arg app_name "$APP_NAME" '.[] | select(.class == $app_name) | .address')
+
+  if [[ -n "$addr" && "$addr" != "null" ]]; then
+    break
+  fi
+
+  sleep 0.1
+done
+
+if [[ -n "$addr" && "$addr" != "null" ]]; then
+  hyprctl dispatch movetoworkspace "special:magic,address:$addr"
+  sleep 0.5
+  hyprctl dispatch togglespecialworkspace magic
+else
+  echo "Window for $APP_NAME not found"
+  exit 1
+fi
